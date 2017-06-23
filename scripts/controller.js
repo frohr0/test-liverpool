29602326f62be87024c2c29d1817fe5844a60b5e
Fidel Rodríguez Hernández <fidel.froh@gmail.com>
2017-06-23 14:22:48 -0500

    Agregando estilos con la funcionalidad

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,31 +1,25 @@
-
-// angular.module("MyFirstApp",[])
-// 	.controller("FirstController",["$scope",function(m){
-// 		m.nombre = "Fidel";
-//    m.newComment = {};
-// 		m.comments = [
-// 			{
-//         userName : "Batman",
-//         comment : "las tortillas estan frias Robin"
-//       },
-//       {
-//         userName : "Spiderman",
-//         comment : "Pongo las pizzas"
-//       },
-// 		];
-//     m.addComment = function(){
-//       m.comments.push(m.newComment);
-//       m.newComment = {};
-//     }
-// 	}]);
-
 var app = angular.module('finderLvp', []);
 app.controller('FirstController', function($scope, $http) {
-    $scope.results = [];
-    $scope.
-    $http.get("https://www.liverpool.com.mx/tienda?s=xbox&d3106047a194921c01969dfdec083925=json")
+  $scope.listResults = [];
+  $scope.latestSearch = [];
+  $scope.newSearch = "";
+  $scope.objSearch = {};
+  $scope.date = new Date();
+  
+  $scope.searchProduct = function(){
+    $http.get("https://www.liverpool.com.mx/tienda?s="+$scope.newSearch+"&d3106047a194921c01969dfdec083925=json")
     .then(function(response) {
-        console.log(response.data.contents[0].mainContent[3].contents[0].records.attributes.sku.smallImage[0]);
-        
+      $scope.listResults = response.data.contents[0].mainContent[3].contents[0].records;
+        // console.log(response.data.contents[0].mainContent[3].contents[0].records);
+        // console.log(response.data.contents[0].mainContent[3].contents[0].records[0].attributes["sku.smallImage"][0]);
+        console.log(response.data.contents[0].mainContent[3].contents[0].records);
+
+        $scope.objSearch.data = $scope.newSearch;
+        $scope.objSearch.date = $scope.date;
+
+        $scope.latestSearch.push($scope.objSearch);
+        $scope.newSearch = "";
+        $scope.objSearch = {};
     });
+  }
 });
