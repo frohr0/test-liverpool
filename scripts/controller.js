bf08f63e4dfbaa590650285866fe853b8ef599bf
Fidel Rodríguez Hernández <fidel.froh@gmail.com>
2017-06-23 16:10:43 -0500

    prueba terminada

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,25 +1,31 @@
-var app = angular.module('finderLvp', []);
-app.controller('FirstController', function($scope, $http) {
+var app = angular.module('finderLvp', ["LocalStorageModule"]);
+app.controller('FirstController', function($scope, $http, localStorageService) {
   $scope.listResults = [];
-  $scope.latestSearch = [];
+  if(localStorageService.get("latest-search")){
+    $scope.latestSearch = localStorageService.get("latest-search");  
+  }else{
+    $scope.latestSearch = [];
+  }
+  // $scope.latestSearch = [];
   $scope.newSearch = "";
   $scope.objSearch = {};
   $scope.date = new Date();
-  
+
+
   $scope.searchProduct = function(){
     $http.get("https://www.liverpool.com.mx/tienda?s="+$scope.newSearch+"&d3106047a194921c01969dfdec083925=json")
     .then(function(response) {
       $scope.listResults = response.data.contents[0].mainContent[3].contents[0].records;
-        // console.log(response.data.contents[0].mainContent[3].contents[0].records);
-        // console.log(response.data.contents[0].mainContent[3].contents[0].records[0].attributes["sku.smallImage"][0]);
-        console.log(response.data.contents[0].mainContent[3].contents[0].records);
-
         $scope.objSearch.data = $scope.newSearch;
         $scope.objSearch.date = $scope.date;
-
         $scope.latestSearch.push($scope.objSearch);
+        localStorageService.set("latest-search",$scope.latestSearch)
         $scope.newSearch = "";
         $scope.objSearch = {};
     });
+  }
+  $scope.clearLatest = function(){
+    $scope.latestSearch = [];
+    localStorageService.set("latest-search",$scope.latestSearch)
   }
 });
--- scripts/testJs.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('MyFirstApp', []);
-app.controller('FirstController', function($scope, $http) {
-    $http.get("https://www.liverpool.com.mx/tienda?s=xbox&d3106047a194921c01969dfdec083925=json")
-    .then(function(response) {
-        console.log(response.data.contents[0].mainContent[3].contents[0].records);
-    });
-});
